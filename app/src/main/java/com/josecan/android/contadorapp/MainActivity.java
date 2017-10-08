9df3f4aedbe05112478dafd58ea3c8b3997e07b9
jose.can <jose.can@itrends.com.mx>
2017-10-08 08:58:59 -0500

    Agregando funcionalidad al proyecto. Botones Contar y Reiniciar

--- app/src/main/java/com/josecan/android/contadorapp/MainActivity.java
+++ app/src/main/java/com/josecan/android/contadorapp/MainActivity.java
@@ -2,12 +2,35 @@ package com.josecan.android.contadorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    int conteo = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText conteoEditText = (EditText) findViewById(R.id.conteo_edit_text);
+        Button contadorButton = (Button) findViewById(R.id.contador_button);
+        Button reiniciarButton = (Button) findViewById(R.id.reiniciar_button);
+
+        contadorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                conteo ++;
+                conteoEditText.setText(Integer.toString(conteo));
+            }
+        });
+
+        reiniciarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                conteo = 0;
+                conteoEditText.setText(Integer.toString(conteo));
+            }
+        });
     }
 }
